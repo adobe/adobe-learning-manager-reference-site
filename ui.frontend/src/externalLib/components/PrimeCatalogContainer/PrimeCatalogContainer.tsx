880da5ffef7f057dcac7c122a637b11a588ee026
Yogesh Pugalia <pugalia@adobe.com>
2022-02-10 17:01:55 +0530

    Adding module.css and supporting variables

--- ui.frontend/src/externalLib/components/PrimeCatalogContainer/PrimeCatalogContainer.tsx
+++ ui.frontend/src/externalLib/components/PrimeCatalogContainer/PrimeCatalogContainer.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { PrimeCatalogFilters } from "../PrimeCatalogFilters";
 import { PrimeTrainingsContainer } from "../PrimeTrainingsContainer";
-
-
+import styles from "./PrimeCatalogContainer.module.css";
+//import styles from './Button.module.css'
 
 const PrimeCatalogContainer  = () => {
   return (
-    <>
+    <div className={styles.prime}>
       <PrimeCatalogFilters></PrimeCatalogFilters>
       <PrimeTrainingsContainer></PrimeTrainingsContainer>
-    </>
+    </div>
   );
 };
 
