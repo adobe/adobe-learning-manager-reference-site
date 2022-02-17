bd82219fe1f2c8c532fed9ce529149d1ca658bab
Tushar Kalra <tus@Tushars-MacBook-Pro.local>
2022-02-17 11:39:44 +0530

    removed moment

--- ui.frontend/src/externalLib/utils/dateTime.ts
+++ ui.frontend/src/externalLib/utils/dateTime.ts
@@ -1,3 +1,5 @@
+import { METHODS } from "http";
+
 export const convertSecondsToTimeText = (time: number)  => {
 
     if(!time || time == 0) {
@@ -21,4 +23,46 @@ export const convertSecondsToTimeText = (time: number)  => {
     const minsText = mins == 0 ? "" : mins == 1 ? "1 min" : `${mins} mins`;
 
     return `${hoursText} ${minsText}`;
+}
+
+
+export const timeSince = (date: any) => {
+
+    var seconds = Math.floor((new Date().valueOf() - new Date(date).valueOf()) / 1000);
+    
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) == 1 ? "1 Year" : Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) == 1 ? "1 month" : Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) == 1 ? "1 day" : Math.floor(interval) +  " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) == 1 ? "1 hour" : Math.floor(interval) +  " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) == 1 ? "1 minute" : Math.floor(interval) +  " minutes";
+    }
+    return Math.floor(seconds) == 1 ? "1 second" : Math.floor(interval) + " seconds";
+  }
+
+
+  export function modifyTime(dateToModify: string, locale: string) {
+    const local = new Date(dateToModify).toLocaleDateString(locale, {
+        day: "numeric",
+        month: "short",
+        year: "numeric",
+        hour: "2-digit",
+        minute: "2-digit",
+    });
+
+    return local;
 }
