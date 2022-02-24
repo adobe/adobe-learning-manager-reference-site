0692c31c6d971131ad4781ea6965cdbca419d29f
Yogesh Pugalia <pugalia@adobe.com>
2022-02-24 17:24:10 +0530

    Fixing instances filtering

--- ui.frontend/src/externalLib/utils/hooks.tsx
+++ ui.frontend/src/externalLib/utils/hooks.tsx
@@ -114,7 +114,7 @@ const filterTrainingInstance = (training: PrimeLearningObject, instanceId: strin
         if(instanceId) {
           return instance.id === instanceId;
         } else {
-          return instance.isDefault;
+          return instance.isDefault && instance.state == "Active";
         }
       }
     );
