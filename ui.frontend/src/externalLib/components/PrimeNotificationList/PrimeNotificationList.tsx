3d81d05599cde9f0ca09b2f6b58d73ad479b3560
Tushar Kalra <tus@Tushars-MacBook-Pro.local>
2022-02-28 22:19:37 +0530

    css for notification

--- ui.frontend/src/externalLib/components/PrimeNotificationList/PrimeNotificationList.tsx
+++ ui.frontend/src/externalLib/components/PrimeNotificationList/PrimeNotificationList.tsx
@@ -5,7 +5,7 @@ import { useLoadMore } from "../../hooks/loadMore";
 import styles from "./PrimeNotificationList.module.css";
 
 const PrimeNotificationsList = (props: any) => {
-    const { notifications, loadMoreNotifications } = props;
+    const { notifications, loadMoreNotifications , unreadCount} = props;
     const elementRef = useRef(null);
     useLoadMore({
       items: notifications,
@@ -14,6 +14,15 @@ const PrimeNotificationsList = (props: any) => {
       elementRef,
     });
     return (
+      <div>
+      <div className={styles.unreadCountItem}>
+          <div className={styles.unreadCountText}>
+            {unreadCount} New
+          </div>
+          <div className={styles.unreadCountNotificationText}>
+            Notifications
+          </div>
+      </div>
       <div className={styles.notificationContainer} id="notifications">
         <ul className={styles.notificationList}>
           {notifications?.map((entry: any) => (
@@ -23,7 +32,8 @@ const PrimeNotificationsList = (props: any) => {
             ></PrimeNotificationItem>
           ))}
         </ul>
-        <div ref={elementRef}>TO DO add Loading more..</div>
+        <div ref={elementRef}></div>
+      </div>
       </div>
     );
   };
