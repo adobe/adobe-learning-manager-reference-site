c15610a0c5be9c8a7ab1cced706ab2e09bf801e3
Tushar Kalra <tus@Tushars-MacBook-Pro.local>
2022-02-28 17:54:12 +0530

    notification css

--- ui.frontend/src/externalLib/components/PrimeNotificationItem/PrimeNotificationItem.tsx
+++ ui.frontend/src/externalLib/components/PrimeNotificationItem/PrimeNotificationItem.tsx
@@ -11,28 +11,34 @@ import { PrimeNotificationText } from "../PrimeNotificationText";
 const PrimeNotificationItem = (props: any) => {
     const notification = props.notification; 
     const messageTime = notification.dateCreated;
-     
+    if (notification && notification.read) {
     return (
         <li className={styles.notificationItem}>
-            {/* <div className={styles.notificationIcon}>
-                {NOTIFICATION_ICON_SVG()}
-            </div> */}
-            <div className={styles.notificationText}>
+            <div>
                 <div>
-                    <PrimeNotificationText notification={notification}>
-                    
+                    <PrimeNotificationText notification={notification}>    
                     </PrimeNotificationText>
                 </div>
                 <div className={styles.notificationTime}>
-                    {/* {
-                     {modifyTimeDDMMYY(messageTime, "en-US")} 
-                    } */}
                     {modifyTime(messageTime, "en-US")}
-                    {/* {timeSince(messageTime)} ago */}
                 </div>
             </div>
         </li>
-        
     );
+    } else {
+        return (
+        <li className={styles.notificationItemUnread}>
+            <div>
+                <div>
+                    <PrimeNotificationText notification={notification}>    
+                    </PrimeNotificationText>
+                </div>
+                <div className={styles.notificationTime}>
+                    {modifyTime(messageTime, "en-US")}
+                </div>
+            </div>
+        </li>
+        )
+    }
 };
 export default PrimeNotificationItem
