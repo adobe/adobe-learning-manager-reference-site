f07ea84cb2fbf7c717fcfec6f93fc18d12eb5477
Ashwin Todurkar <todurkar@adobe.com>
2022-03-13 02:59:08 +0530

    moving back to community folder

---
+++ ui.frontend/src/externalLib/components/Community/PrimeCommunityBoard/PrimeCommunityBoard.tsx
@@ -0,0 +1,188 @@
+import {
+  SOCIAL_MORE_OPTIONS_SVG, 
+  SOCIAL_ACTIVITY_INDEX_HIGH_SVG,
+  SOCIAL_ACTIVITY_INDEX_MEDIUM_SVG,
+  SOCIAL_ACTIVITY_INDEX_LOW_SVG,
+} from "../../../utils/inline_svg"
+import { PrimeCommunityBoardOptions } from "../PrimeCommunityBoardOptions";
+import { PrimeAlertDialog } from "../PrimeAlertDialog";
+import { formatDate } from "../../../utils/dateTime";
+import GlobeOutline from "@spectrum-icons/workflow/GlobeOutline"
+import LockOpen from "@spectrum-icons/workflow/LockOpen"
+import LockClosed from "@spectrum-icons/workflow/LockClosed"
+import Info from "@spectrum-icons/workflow/Info"
+import FileTxt from "@spectrum-icons/workflow/FileTxt"
+import Visibility from "@spectrum-icons/workflow/Visibility"
+import UserGroup from "@spectrum-icons/workflow/UserGroup"
+import Clock from "@spectrum-icons/workflow/Clock"
+import { getALMObject } from "../../../utils/global";
+import { useState } from "react";
+import { useIntl } from "react-intl";
+import { useBoardOptions } from "../../../hooks/community";
+import { PrimeCommunityObjectBody } from "../PrimeCommunityObjectBody";
+import styles from "./PrimeCommunityBoard.module.css";
+
+const PrimeCommunityBoard  = (props: any) => {
+  const { formatMessage } = useIntl();
+  let board = props.board;
+  let showBoardOptions = false;
+  const [isBoardOptionsOpen, setIsBoardOptionsOpen] = useState(showBoardOptions);
+  const { reportBoard } = useBoardOptions();
+  const [ showConfirmation, setShowConfirmation ] = useState(false);
+  
+  const boardSkills = board.skills.map((skill: any, index: any) => {
+    return (index ? ', ': '') + skill.name;
+  });
+
+  const toggleBoardOptionsHandler = () => {
+    setIsBoardOptionsOpen((prevState) => !prevState);
+  };
+
+  const boardNameClickHandler = () => {
+    console.log(board.id);
+    getALMObject().navigateToBoardPage(board.id);
+  }
+
+  const reportBoardHandler = () => {
+    setShowConfirmation(true);
+  }
+
+  const callReportBoard = async() => {
+    try {
+      await reportBoard(board.id);
+    } catch(Exception){};
+    hideConfirmationDialog();
+  }
+
+  const hideConfirmationDialog = () => {
+      setShowConfirmation(false);
+  }
+
+  return (
+    <>
+    <div className={styles.primeBoardWrapper}>
+      <div className={styles.primeBoardItem}>
+        <div className="prime-title-skills-container">
+          {/* <span id="sr-only">${i18n(state.locale).BOARD}</span> */}
+          <button className={styles.primeBoardOptions} onClick={toggleBoardOptionsHandler} id={"prime-board-options-" + board.id}>
+            {SOCIAL_MORE_OPTIONS_SVG()}
+            {isBoardOptionsOpen && 
+              <PrimeCommunityBoardOptions board={board} boardOptionsHandler={toggleBoardOptionsHandler} reportBoardHandler={reportBoardHandler}></PrimeCommunityBoardOptions>
+            }  
+          </button>
+          {showConfirmation &&
+            <PrimeAlertDialog
+            variant="confirmation"
+            title="Confirmation Required"
+            primaryActionLabel="Report"
+            onPrimaryAction={callReportBoard}
+            secondaryActionLabel="Cancel"
+            onSecondaryAction={hideConfirmationDialog}
+            body="Are you sure you want to report this board? A notification will be sent to the board administrator and moderators."
+            ></PrimeAlertDialog>
+          }
+          <div className={styles.primeBoardName} role="link" tabIndex={0} onClick={boardNameClickHandler}>{board.name}</div>
+          <div className={styles.primeBoardSkill}>
+            {formatMessage({
+              id: "prime.community.board.skills",
+              defaultMessage: "Skills",
+            })}: {boardSkills}
+            <div className={styles.primeBoardIcon} title={
+              board.visibility === "PUBLIC" ? 
+                formatMessage({
+                  id: "prime.community.board.public",
+                  defaultMessage: "Public Board",
+                }) 
+                : board.visibility === "PRIVATE" ?
+                  formatMessage({
+                    id: "prime.community.board.private",
+                    defaultMessage: "Private Board",
+                  })
+                  :
+                  formatMessage({
+                    id: "prime.community.board.restricted",
+                    defaultMessage: "Restricted Board",
+                })
+              }
+              >  
+              {board.visibility === "PUBLIC" ? <GlobeOutline/> : board.visibility === "PRIVATE" ? <LockClosed/> : <LockOpen/>}
+            </div>
+          </div>
+        </div>
+        <PrimeCommunityObjectBody object={board} type="board"></PrimeCommunityObjectBody>
+        <div className={styles.primeBoardActivityPanel}>
+            <div className={styles.primeBoardActivityStats}>
+                <div className={styles.primeActivityStatsIcon}>
+                  {board.visibility === "HIGH" ? SOCIAL_ACTIVITY_INDEX_HIGH_SVG() : 
+                    board.visibility === "NORMAL" ? SOCIAL_ACTIVITY_INDEX_MEDIUM_SVG() : SOCIAL_ACTIVITY_INDEX_LOW_SVG()}
+                </div>
+                <span className={styles.primeActivityStatsText}>
+                    {board.visibility === "HIGH" ? 
+                    formatMessage({
+                      id: "prime.community.board.highActivity",
+                      defaultMessage: "High Activity",
+                    }) : 
+                      board.visibility === "NORMAL" ? 
+                      formatMessage({
+                        id: "prime.community.board.normalActivity",
+                        defaultMessage: "Normal Activity",
+                      }) : 
+                        formatMessage({
+                          id: "prime.community.board.lowActivity",
+                          defaultMessage: "Low Activity",
+                        })}
+                </span>
+                <div className={styles.primeActivityInfoIcon} data-toggle="tooltip" data-trigger="hover" data-tooltip-position="right" title={
+                  formatMessage({
+                    id: "prime.community.board.activityCalc",
+                    defaultMessage: "Calculated daily based on the number of new posts, comments, participants, views, likes and dislikes",
+                  })}>
+                  {<Info/>}
+                </div>
+            </div>
+            <div className={styles.primeVerticalSeperator}></div>
+            <div className={styles.primeBoardActivityStats}>
+              <div className={styles.primeActivityStatsIcon}>
+                {<FileTxt/>}
+              </div>
+              <span className={styles.primeActivityStatsText}>
+                {board.postCount} {formatMessage({id: "prime.community.board.post.label", defaultMessage: "Post(s)",})}
+              </span>
+            </div>
+            <div className={styles.primeBoardActivityStats}>
+              <div className={styles.primeActivityStatsIcon}>
+                {<Visibility/>}
+              </div>
+              <span className={styles.primeActivityStatsText}>
+                {board.viewsCount} {formatMessage({id: "prime.community.board.view.label", defaultMessage: "View(s)",})}
+              </span>
+            </div>
+            <div className={styles.primeBoardActivityStats}>
+              <div className={styles.primeActivityStatsIcon}>
+                {<UserGroup/>}
+              </div>
+              <span className={styles.primeActivityStatsText}>
+                {board.userCount} {formatMessage({id: "prime.community.board.people.label", defaultMessage: "People",})}
+              </span>
+            </div>
+            <div className={styles.primeVerticalSeperator}></div>
+            <div className={styles.primeBoardActivityStats}>
+              <div className={styles.primeActivityStatsIcon}>
+                {<Clock/>}
+              </div>
+              <span className={styles.primeActivityStatsText}>
+                {formatMessage({id: "prime.community.board.createdOn.label", defaultMessage: "Created on ",})}
+                {formatDate(board.dateCreated)}
+                {formatMessage({id: "prime.community.board.by.label", defaultMessage: " by ",})}
+                {board.createdBy.name !== "" ? board.createdBy.name : 
+                  formatMessage({id: "prime.community.board.anoymous.label", defaultMessage: "Anonymous",})}
+              </span>
+            </div>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+};
+
+export default PrimeCommunityBoard;
---
+++ ui.frontend/src/externalLib/components/Community/PrimeCommunityObjectOptions/PrimeCommunityObjectOptions.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useRef, useState } from "react";
+import { useIntl } from "react-intl";
+import { PrimeUser } from "../../../models";
+import { getALMUser } from "../../../utils/global";
+import styles from "./PrimeCommunityObjectOptions.module.css";
+
+const PrimeCommunityObjectOptions = (props: any) => {
+  const ref = useRef<any>();
+  const [user, setUser] = useState({} as PrimeUser);
+  const { formatMessage } = useIntl();
+
+  useEffect(() => {
+    (async () => {
+      const response = await getALMUser();
+      setUser(response.user);
+    })();
+  }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      console.log(props.object.createdBy.id);
+
+      //console.log("userid=" + user.id);   //to-do user_id is empty
+      if (ref.current && !ref.current.contains(event.target)) {
+        props.toggleOptions && props.toggleOptions();
+      }
+    };
+    document.addEventListener("click", handleClickOutside, true);
+    return () => {
+      document.removeEventListener("click", handleClickOutside, true);
+    };
+  });
+
+  const deleteObjectHandler = () => {
+    if (typeof props.deleteHandler === "function") {
+      props.deleteHandler();
+    }
+  };
+
+  const reportObjectHandler = () => {
+    if (typeof props.reportAbuseHandler === "function") {
+      props.reportAbuseHandler();
+    }
+  };
+
+  const editObjectHandler = () => {
+    if (typeof props.editHandler === "function") {
+      props.editHandler();
+    }
+  };
+
+  return (
+    <>
+      <div ref={ref} className={styles.primeObjectOptionsList}>
+        {props.object.createdBy.id === user.id &&
+          <div className={styles.primeObjectRegularOption} onClick={editObjectHandler}>
+            {
+              formatMessage({
+                id: "prime.community.board.edit",
+                defaultMessage: "Edit",
+              })
+            }
+          </div> 
+        }
+        <div className={styles.primeSeperator}></div>
+        {props.object.createdBy.id === user.id &&
+          <div className={styles.primeObjectCriticalOption} onClick={deleteObjectHandler}>
+            {
+              formatMessage({
+                id: "prime.community.board.delete",
+                defaultMessage: "Delete",
+              })
+            }
+          </div> 
+        }
+        <div className={styles.primeObjectCriticalOption} onClick={reportObjectHandler}>
+          {
+            formatMessage({
+              id: "prime.community.board.report",
+              defaultMessage: "Report",
+            })
+          }
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PrimeCommunityObjectOptions;
