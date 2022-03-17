87b365ca98deceeea7cbadfa5183637bfbd366ef
Praful Kumar Vaishnav <vaishnav@adobe.com>
2022-03-17 11:14:25 +0530

    Add menu header.

--- ui.apps/src/main/content/jcr_root/apps/aem-learn-components/components/header/clientlibs/js/header.js
+++ ui.apps/src/main/content/jcr_root/apps/aem-learn-components/components/header/clientlibs/js/header.js
@@ -2,18 +2,31 @@
 (function (document, window, $) {
     "use strict";
 
+    const HEADER_SHOW_MENU_SEL = ".alm-header-cont .alm-header-menu"
     const HEADER_PROFILE_PIC_SEL = ".alm-header-profile";
+    const VERT_NAV_CONTAINER_SEL = ".alm-header-vertical-nav";
+    const VERT_NAV_CLOSE_BUTTON_SEL = ".vert-nav-close-button"
+    const VERT_NAV_PROFILE_NAME_SEL = ".vert-nav-profile-name";
 
     $(document).ready(function () {
         window.ALM.getALMUser().then(function (user) {
             try 
             {
-                const avatarUrl = JSON.parse(user).data.attributes.avatarUrl;
-                $(HEADER_PROFILE_PIC_SEL).attr("src", avatarUrl);
+                const userAttrs = JSON.parse(user).data.attributes;
+                $(HEADER_PROFILE_PIC_SEL).attr("src", userAttrs.avatarUrl);
+                $(VERT_NAV_PROFILE_NAME_SEL).text(userAttrs.name);
             } catch (e)
             {
                 console.error("Unable to fetch user avatar.");
             }
+        });
+
+        $(document).on("click", VERT_NAV_CLOSE_BUTTON_SEL, function(e) {
+            $(VERT_NAV_CONTAINER_SEL).toggle();
+        });
+
+        $(document).on("click", HEADER_SHOW_MENU_SEL, function(e) {
+            $(VERT_NAV_CONTAINER_SEL).toggle();
         });
     });
 
