b3b0f00f453054e514e200c7e43ff6e4f7669dce
Suraj Negi <sunegi@adobe.com>
2022-03-22 12:07:03 +0530

    Added code to shoiw dialog in overvie when Jobaid is not enrolled.

--- ui.frontend/src/externalLib/components/Community/PrimeAlertDialog/PrimeAlertDialog.tsx
+++ ui.frontend/src/externalLib/components/Community/PrimeAlertDialog/PrimeAlertDialog.tsx
@@ -1,58 +1,63 @@
 import {
-    ActionButton,
-    DialogTrigger,
-    Provider,
-    lightTheme,
-    AlertDialog
-  } from "@adobe/react-spectrum";
+  ActionButton,
+  DialogTrigger,
+  Provider,
+  lightTheme,
+  AlertDialog,
+} from "@adobe/react-spectrum";
 import { useEffect, useRef } from "react";
 import styles from "./PrimeAlertDialog.module.css";
-  
+
 const PrimeAlertDialog = (props: any) => {
-    const showDialog = useRef(false);
+  const showDialog = useRef(false);
 
-    useEffect(() => {
-        if(!showDialog.current) {
-            const launchDialog = document.getElementById("showAlert") as HTMLElement;
-            launchDialog.click();
-            showDialog.current = true;
-        }
-    }, [showDialog]);
-
-    const hideDialog = () => {
-        showDialog.current = false;
+  useEffect(() => {
+    if (!showDialog.current) {
+      const launchDialog = document.getElementById("showAlert") as HTMLElement;
+      launchDialog.click();
+      showDialog.current = true;
     }
-    const onPrimaryActionHandler = () => {
-        hideDialog();
-        if(typeof props.onPrimaryAction === 'function') {
-            props.onPrimaryAction();
-        }
-    }
+  }, [showDialog]);
 
-    const onSecondaryActionHandler = () => {
-        hideDialog();
-        if(typeof props.onSecondaryAction === 'function') {
-            props.onSecondaryAction();
-        }
-    }
-
-    return (
-        <Provider theme={lightTheme} colorScheme={"light"}>
-            <DialogTrigger>
-                <ActionButton id="showAlert" UNSAFE_className={styles.primeAlertDialogButton}></ActionButton>
-                <AlertDialog
-                    variant={props.variant}
-                    title={props.title}
-                    primaryActionLabel={props.primaryActionLabel}
-                    onPrimaryAction={onPrimaryActionHandler}
-                    secondaryActionLabel={props.secondaryActionLabel}
-                    onSecondaryAction={onSecondaryActionHandler}
-                    autoFocusButton="primary"
-                >{props.body}
-                </AlertDialog>
-            </DialogTrigger>
-        </Provider>
-    );
+  const hideDialog = () => {
+    showDialog.current = false;
   };
-  
-  export default PrimeAlertDialog;
+  const onPrimaryActionHandler = () => {
+    hideDialog();
+    if (typeof props.onPrimaryAction === "function") {
+      props.onPrimaryAction();
+    }
+  };
+
+  const onSecondaryActionHandler = () => {
+    hideDialog();
+    if (typeof props.onSecondaryAction === "function") {
+      props.onSecondaryAction();
+    }
+  };
+
+  return (
+    <Provider theme={lightTheme} colorScheme={"light"}>
+      <DialogTrigger>
+        <ActionButton
+          id="showAlert"
+          UNSAFE_className={styles.primeAlertDialogButton}
+        ></ActionButton>
+        <AlertDialog
+          variant={props.variant}
+          title={props.title}
+          primaryActionLabel={props.primaryActionLabel}
+          onPrimaryAction={onPrimaryActionHandler}
+          secondaryActionLabel={props.secondaryActionLabel}
+          onSecondaryAction={onSecondaryActionHandler}
+          autoFocusButton="primary"
+          UNSAFE_className={props.classes || ""}
+        >
+          {props.body}
+        </AlertDialog>
+      </DialogTrigger>
+    </Provider>
+  );
+};
+
+export default PrimeAlertDialog;
