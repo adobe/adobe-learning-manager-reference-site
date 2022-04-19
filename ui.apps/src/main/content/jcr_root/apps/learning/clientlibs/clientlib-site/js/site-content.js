4085764e59ee91c889bfdc27f7c39866b2be7639
Praful Kumar Vaishnav <vaishnav@adobe.com>
2022-04-19 16:43:57 +0530

    Update signout page.

--- ui.apps/src/main/content/jcr_root/apps/learning/clientlibs/clientlib-site/js/site-content.js
+++ ui.apps/src/main/content/jcr_root/apps/learning/clientlibs/clientlib-site/js/site-content.js
@@ -2,12 +2,7 @@
   "use strict";
 
   const FOOTER_TOP_SCROLL_SEL = "#alm-footer-scrollTop";
-
-  const CONFIG_META_TAG_NAME = "cp-config";
-
-  const getALMConfig = () => {
-    return window.ALM.ALMConfig;
-  };
+  const SIGNOUT_PAGE_LOGIN_BTN_SEL = "#signout-page-login-btn";
 
   const handleFooterScrollToTop = () => {
     $(FOOTER_TOP_SCROLL_SEL).on("click", () => window.scrollTo({
@@ -18,6 +13,7 @@
   
   $(document).ready(function () {
     handleFooterScrollToTop();
+    $(SIGNOUT_PAGE_LOGIN_BTN_SEL).on("click", () => window.ALM.navigateToHomePage());
   });
 
 })(window, document, Granite, jQuery);
