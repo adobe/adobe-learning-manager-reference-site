22c0a4ff1748231af2a7a74fcaec6c3c209c0c60
Yogesh Pugalia <pugalia@adobe.com>
2022-06-10 11:30:39 +0530

    CPRIME-55208

--- ui.frontend/src/almLib/components/Community/PrimeCommunityBoardsContainer/PrimeCommunityBoardsContainer.tsx
+++ ui.frontend/src/almLib/components/Community/PrimeCommunityBoardsContainer/PrimeCommunityBoardsContainer.tsx
@@ -9,7 +9,7 @@ the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTA
 OF ANY KIND, either express or implied. See the License for the specific language
 governing permissions and limitations under the License.
 */
-import { Button, lightTheme, Provider } from "@adobe/react-spectrum";
+import { lightTheme, Provider } from "@adobe/react-spectrum";
 import { useIntl } from "react-intl";
 import { PrimeBoard } from "../../../models/PrimeModels";
 import { PrimeCommunityBoard } from "../PrimeCommunityBoard";
